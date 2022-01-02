94b2c86529d8ecba3236dcc6df7b82a1e0e4e45b
daanzu <daanzu@gmail.com>
2022-01-02 02:46:58 -0500

    add WenetSTTDecoder

--- src/wenet_active_grammar/wrapper.py
+++ src/wenet_active_grammar/wrapper.py
@@ -85,6 +85,66 @@ class WenetSTTModel(FFIObject):
             raise WenetError("text may be too long")
         return text.strip()
 
+class WenetSTTDecoder(FFIObject):
+
+    _library_header_text = """
+        WENET_STT_API void *wenet_stt__construct_decoder(void *model_vp);
+        WENET_STT_API bool wenet_stt__destruct_decoder(void *decoder_vp);
+        WENET_STT_API bool wenet_stt__decode(void *decoder_vp, float *wav_samples, int32_t wav_samples_len, bool finalize);
+        WENET_STT_API bool wenet_stt__get_result(void *decoder_vp, char *text, int32_t text_max_len, bool *final_p);
+        WENET_STT_API bool wenet_stt__reset(void *decoder_vp);
+    """
+
+    def __init__(self, model):
+        if not isinstance(model, WenetSTTModel):
+            raise TypeError("model must be a WenetSTTModel")
+
+        super().__init__()
+        result = self._lib.wenet_stt__construct_decoder(model._model)
+        if result == _ffi.NULL:
+            raise WenetError("wenet_stt__construct_decoder failed")
+        self._decoder = result
+
+    def __del__(self):
+        if hasattr(self, '_decoder'):
+            result = self._lib.wenet_stt__destruct_decoder(self._decoder)
+            if not result:
+                raise WenetError("wenet_stt__destruct_decoder failed")
+
+    def decode(self, wav_samples, finalize):
+        if not isinstance(wav_samples, np.ndarray): wav_samples = np.frombuffer(wav_samples, np.int16)
+        wav_samples = wav_samples.astype(np.float32)
+        wav_samples_char = _ffi.from_buffer(wav_samples)
+        wav_samples_float = _ffi.cast('float *', wav_samples_char)
+        finalize = bool(finalize)
+
+        result = self._lib.wenet_stt__decode(self._decoder, wav_samples_float, len(wav_samples), finalize)
+        if not result:
+            raise WenetError("wenet_stt__decode failed")
+
+    def get_result(self, final=None, text_max_len=1024):
+        text_p = _ffi.new('char[]', text_max_len)
+        result_final_p = _ffi.new('bool *')
+
+        while True:
+            result = self._lib.wenet_stt__get_result(self._decoder, text_p, text_max_len, result_final_p)
+            if not result:
+                raise WenetError("wenet_stt__get_result failed")
+            result_final = bool(result_final_p[0])
+            if not final or result_final:
+                break
+            time.sleep(0.01)
+
+        text = decode(_ffi.string(text_p))
+        if len(text) >= (text_max_len - 1):
+            raise WenetError("text may be too long")
+        return text.strip(), result_final
+
+    def reset(self):
+        result = self._lib.wenet_stt__reset(self._decoder)
+        if not result:
+            raise WenetError("wenet_stt__reset failed")
+
 class WenetAGDecoder(FFIObject):
 
     _library_header_text = """
